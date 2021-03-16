3a2984b3dce8481da8093d9123b0eb327fe80309
unknown <crizverar@gmail.com>
2021-03-16 17:00:08 -0300

    commit

---
+++ assets/javaScript/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.navigation');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menu.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menu.classList.remove('open');
+        menuOpen = false;
+    }
+});
